a9f86b29ea0f6af47494eb42d9cfff614148fc64
Taimoor Pasha <46814549+taimoorpashanbs17@users.noreply.github.com>
2020-05-04 04:04:00 +0500

    Test Files have been added

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from app.calc import sum 
+
+class CalcTests(TestCase):
+    def test_1_sum(self):
+        "Check That sum should be Same"
+        self.assertEqual(sum(4,5), 9)
+
+    def test_2_invalid_sum(self):
+        "We enter values and check results not same"
+        self.assertEqual(sum(3,4), 10)
+
+    def test_3_valid_sum(self):
+        "We enter values and check results not same"
+        self.assertEqual(sum(3,4), 7)
