8ae087c3b3a6d37b2c6627c87c03aed9179a1167
Taimoor Pasha <46814549+taimoorpashanbs17@users.noreply.github.com>
2020-05-04 20:57:33 +0500

    Latest Changes

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 
 class UserManager(BaseUserManager):
-    
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new model. """
         user = self.model(email=email, **extra_fields)
